ac285bf742e16c24a4c702d37a5956fad0f0b80b
quangvanviet <quangvanviet.mana@gmail.com>
2025-10-13 16:31:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -59,21 +59,22 @@ window.addEventListener("resize", () => {
 });
 
 // 💌 Lời chúc
-// Import Firebase
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-  import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+// Cấu hình Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDWY3sQ_Gz4UeKGqFHKp10d83h3SRNoZv0",
+  authDomain: "wedding-card-be153.firebaseapp.com",
+  databaseURL: "https://wedding-card-be153-default-rtdb.firebaseio.com",
+  projectId: "wedding-card-be153",
+  storageBucket: "wedding-card-be153.firebasestorage.app",
+  messagingSenderId: "396090536315",
+  appId: "1:396090536315:web:9ad4ce0da0bfe2c86f0dde",
+  measurementId: "G-TLF5PVGE8N"
+};
 
-  // === CONFIG CỦA BẠN ===
-  const firebaseConfig = {
-    apiKey: "AIzaSyDWY3sQ_Gz4UeKGqFHKp10d83h3SRNoZv0",
-    authDomain: "wedding-card-be153.firebaseapp.com",
-    databaseURL: "https://wedding-card-be153-default-rtdb.firebaseio.com",
-    projectId: "wedding-card-be153",
-    storageBucket: "wedding-card-be153.firebasestorage.app",
-    messagingSenderId: "396090536315",
-    appId: "1:396090536315:web:9ad4ce0da0bfe2c86f0dde",
-    measurementId: "G-TLF5PVGE8N"
-  };
+// Khởi tạo Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const analytics = firebase.analytics();
 
   // === KẾT NỐI FIREBASE ===
   const app = initializeApp(firebaseConfig);
